edf350e5620aa0c825b1daadea2eaaeb30930355
zoothezoo <kohekoshien@gmail.com>
2020-05-09 03:11:45 +0900

    set title and back button

--- app/src/main/java/com/example/groupieplayground/MainActivity.kt
+++ app/src/main/java/com/example/groupieplayground/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.groupieplayground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+
 }
