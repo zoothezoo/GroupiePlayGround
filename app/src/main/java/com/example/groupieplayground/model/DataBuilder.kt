f5adb11be5f74db84d3bd2504ca97ed763b46f81
zoothezoo <kohekoshien@gmail.com>
2020-05-09 02:07:02 +0900

    implement data

---
+++ app/src/main/java/com/example/groupieplayground/model/DataBuilder.kt
@@ -0,0 +1,47 @@
+package com.example.groupieplayground.model
+
+class DataBuilder {
+    val names = listOf<String>(
+        "Ando",
+        "Bando",
+        "Chihiro",
+        "Daisuke",
+        "Eri",
+        "Fujikawa",
+        "Gushiken",
+        "Hayato",
+        "Ikeda",
+        "Jiro"
+    )
+
+    val ages = listOf<Int>(
+        22, 30, 12, 34, 23, 41, 15, 19, 28, 30
+    )
+
+    val hobbies = listOf<String>(
+        "baseball",
+        "music",
+        "running",
+        "programming",
+        "cooking",
+        "reading",
+        "movie",
+        "sleeping",
+        "youtube",
+        "talking"
+    )
+
+    fun makePersonList(): List<Person> {
+        val list = mutableListOf<Person>()
+        for (i in 0..10) {
+            list.add(
+                Person(
+                    name = names[i],
+                    age = ages[i],
+                    hobby = hobbies[i]
+                )
+            )
+        }
+        return list.toList()
+    }
+}
---
+++ app/src/main/java/com/example/groupieplayground/model/Person.kt
@@ -0,0 +1,8 @@
+package com.example.groupieplayground.model
+
+data class Person (
+    val name: String,
+    val age: Int,
+    val hobby: String
+)
+
