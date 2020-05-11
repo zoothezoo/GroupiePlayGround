1bb355749918ec0b831374e94a6d8b778165085d
zoothezoo <kohekoshien@gmail.com>
2020-05-11 16:35:37 +0900

    update: Models

--- app/src/main/java/com/example/groupieplayground/model/DataBuilder.kt
+++ app/src/main/java/com/example/groupieplayground/model/DataBuilder.kt
@@ -1,47 +1,11 @@
 package com.example.groupieplayground.model
 
-class DataBuilder {
-    val names = listOf<String>(
-        "Ando",
-        "Bando",
-        "Chihiro",
-        "Daisuke",
-        "Eri",
-        "Fujikawa",
-        "Gushiken",
-        "Hayato",
-        "Ikeda",
-        "Jiro"
-    )
+interface DataBuilder {
 
-    val ages = listOf<Int>(
-        22, 30, 12, 34, 23, 41, 15, 19, 28, 30
-    )
+    fun buildAllPeople(): List<Person>
 
-    val hobbies = listOf<String>(
-        "baseball",
-        "music",
-        "running",
-        "programming",
-        "cooking",
-        "reading",
-        "movie",
-        "sleeping",
-        "youtube",
-        "talking"
-    )
+    fun buildFavoritePeople(): List<Person>
 
-    fun makePersonList(): List<Person> {
-        val list = mutableListOf<Person>()
-        for (i in 0..9) {
-            list.add(
-                Person(
-                    name = names[i],
-                    age = ages[i],
-                    hobby = hobbies[i]
-                )
-            )
-        }
-        return list.toList()
-    }
+    fun buildNormalPeople(): List<Person>
+
 }
